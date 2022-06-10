f1d4251d93a72ebd5645d478f4b8df8a1ccc2900
olgahomenkova <olenkahoma@gmail.com>
2022-06-10 17:57:00 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+
+        int speedOfPlayer = 5;
+
+        boolean isGreenLight = false;
+
+        if (isGreenLight) {
+            System.out.println("Не выбыл");
+        } else {
+            if (speedOfPlayer == 0) {
+                System.out.println("Не выбыл");
+            }
+            if (speedOfPlayer >= 1) {
+                System.out.println("Выбыл");
+            }
+        }
+    }
+}
