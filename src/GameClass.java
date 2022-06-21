5b802737d149f8c9a51b1b65acee817678733a80
olgahomenkova <olenkahoma@gmail.com>
2022-06-21 17:21:17 +0200

    homework16

---
+++ src/GameClass.java
@@ -0,0 +1,9 @@
+public class GameClass {
+    public static boolean passes(int speed) {
+        boolean isGreenLight = false;
+        if (isGreenLight) {
+            return true;
+        }
+        return speed == 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 class Main {
     public static void main(String[] args) {
 
-        int speedOfPlayer = 5;
-
-        boolean isGreenLight = false;
-
-        if (isGreenLight) {
-            System.out.println("Не выбыл");
-        } else {
-            if (speedOfPlayer == 0) {
-                System.out.println("Не выбыл");
-            }
-            if (speedOfPlayer >= 1) {
-                System.out.println("Выбыл");
-            }
-        }
+        int speedOfPlayer = 0;
+        boolean res = GameClass.passes(speedOfPlayer);
+        System.out.println("Result: " + res);
     }
 }
